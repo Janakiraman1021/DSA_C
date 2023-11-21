c5866e93eea96cd73f2b4358f95a1e0b2a684d5e
JANAKIRAMAN <janakiramankeerthivelan1021@gmail.com>
2023-11-21 23:17:38 +0530

    updated insertion sort

---
+++ inserion.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+void insertion_sort(int arr[] ,int n);
+
+void main{
+    int arr[10],i,n;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    insertion_sort(arr,n);
+    for(i=0;i<n;i++){
+        printf("%d \t ",arr[i]);
+    }
+
+}
+
+void insertion_sort(int arr[] , int n ){
+    int i,j,temp;
+    for(i=1;i<n;i++){
+        temp = arr[i];
+        j= i-1;
+        while((temp < arr[j])&&(j>=0)){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
+
+
+/*
+25, 4 , 52 , 36 
+
+ i =1
+ j = j-1 = j=0
+arr[i] = 4
+arr[j] = 25 
+temp = 25 
+
+arr[j+1] = arr[j] => arr[0+1] = arr[0] => arr[1] = 25
+j-- =>  j = 0-1 => j = -1 exits the while loop 
+
+arr[j+1] => arr[-1+1] => arr[0] = 4
+
+increase i value by 1 for FOR loop 
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+*/
--- new.c
+++ new.c
@@ -5,15 +5,15 @@ int main() {
     printf("\n Enter the number of elements in the array : ");
     scanf("%d",&n);
     printf("\n Enter the Elements : ");
-    for(i=0;i<=n-1;i++){
+    for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
     printf("\n The Elements are : ");
         for(i=0;i<=n-1;i++){
            printf("%d",arr[i]);
     }
-    for(i=0;i<n-1;i++){
-        for(j=0;j<-i-1,j++){
+    for(i=0;i<=n-1;i++){
+        for(j=0;j< = n-i-1;j++){
             if (arr[j]>arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
